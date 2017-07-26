e5c16c4be1a10df8510ae17d2006487116a14638
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2017-07-25 19:57:37 -0500

    Final Commit.

--- CreditLimitApp/Program.cs
+++ CreditLimitApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Joshua Pickenpaugh
 //July 25th, 2017
-//Credit Limit Calculator
+//Simple Console Credit Limit Calculator
 
 using System;
 using System.Collections.Generic;
@@ -14,6 +14,50 @@ namespace CreditLimitApp
     {
         static void Main(string[] args)
         {
+            int intChoice = 1;
+
+            while (intChoice == 1)
+            {
+                double dblCustomerTotal = 0.00;
+                string strCustomerTotal;
+
+                Console.Write("Enter balance from start of month:");
+                double dblStartMonthBalance = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter total of all items charged:");
+                double dblTotalItems = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter number of credit for customer this month:");
+                double dblGivenCredits = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter allowed credit limit:");
+                double dblAllowedCreditLimit = Convert.ToInt32(Console.ReadLine());
+
+                dblCustomerTotal = dblAllowedCreditLimit - ((dblTotalItems - dblGivenCredits) +
+                    dblStartMonthBalance);
+
+                if (dblCustomerTotal > 0)
+                {
+                    strCustomerTotal = dblCustomerTotal.ToString("N2");
+                    Console.WriteLine("Customer has {0} credit remaining.", strCustomerTotal);
+                }
+                else if (dblCustomerTotal == 0)
+                {
+                    strCustomerTotal = dblCustomerTotal.ToString("N2");
+                    Console.WriteLine("Customer has no credits remaining.");
+                }
+                else
+                {
+                    strCustomerTotal = dblCustomerTotal.ToString("N2");
+                    Console.WriteLine("Customer is {0} over credit.", strCustomerTotal);
+                }
+
+                Console.WriteLine("_____________________________________");
+                Console.WriteLine("1. Enter another customer.");
+                Console.WriteLine("2. EXIT");
+                intChoice = Convert.ToInt32(Console.ReadLine());
+            }
+
         }
     }
 }
